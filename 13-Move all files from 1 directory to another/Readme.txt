7be9b9d391454e766c966a5e04e502d57806aff4
Shubhaankar <cshubhaankar@gmail.com>
2017-09-10 13:48:43 +0530

    Add files via upload

---
+++ 13-Move all files from 1 directory to another/Readme.txt
@@ -0,0 +1,2 @@
+Moves files from 1 directory to another.
+User needs to pass both directories as a commandline argument.
--- 13-Move all files from 1 directory to another/header.h
+++ 13-Move all files from 1 directory to another/header.h
@@ -1,1 +1,9 @@
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
